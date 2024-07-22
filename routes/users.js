5346d439f335cdf7c6f1bf842b66e28e0aafa1fe
keshav.gaur <keshav.gaur@darwinbox.io>
2024-07-22 13:51:19 +0530

    updated logger config

--- routes/users.js
+++ routes/users.js
@@ -37,14 +37,12 @@ router.get('/', (req, res, next) => {
     logger.debug('here: In get users');
     try {
         const users = {
-            users: [{
-                name: 'Keshav',
-                email: 'keshav@infoedge.com',
-            },
+          users: [
             {
-                name: 'ankit pareek',
-                email: 'ankit.pareek.@infoedge.com',
-            }],
+              name: "Keshav",
+              email: "keshav.gaur@test.com",
+            },
+          ],
         };
         logger.debug('here: Got users', users);
         logger.info('Got users:');
@@ -78,10 +76,6 @@ router.get('/fake-error', (req, res, next) => {
         logger.error(e);
         next(new HttpError(HttpStatusCode.INTERNAL_SERVER_ERROR, { stack: e }));
     }
-});
-
-router.post('/validate', validate(require('../validators/username')), (req, res, next) => {
-    res.status(200).json({ username: req.body.username });
 });
 
 module.exports = router;
